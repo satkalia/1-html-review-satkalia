b085803179e0db8927e94d87b1ec91173f0edc50
Satvika Kalia <satkalia@iu.edu>
2021-10-20 06:42:02 -0400

    Update book.js

--- app/public/js/books.js
+++ app/public/js/books.js
@@ -3,6 +3,8 @@ const SomeApp = {
     data() {
       return {
         book: [],
+        bookForm: {},
+        selectedBook: null
       }
     },
     computed: {},
@@ -21,6 +23,28 @@ const SomeApp = {
             .catch( (err) => {
                 console.error(err);
             })
+        },
+        postAddBook(evt) {
+            //this.bookForm.bookId = this.selectedBook.id;
+            console.log("Posting:", this.bookForm);
+            //alert("Posting!");
+
+            fetch('api/book/create.php',{
+                method:'POST',
+                body: JSON.stringify(this.bookForm),
+                headers: {
+                    "Content-Type": "application/json; charset=utf-8"
+                }
+            })
+           .then(response => response.json())
+           .then(json => {
+               console.log("Returned from post:", json);
+               //TOFO: test a result was returned!
+               this.books = json;
+
+               //reset the form
+               this.bookForm = {};
+           }); 
         }
     },
     created() {
