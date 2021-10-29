f8517a5bce524a0e27fb96b1fb1e62ebca7cfdaf
Satvika Kalia <satkalia@iu.edu>
2021-10-29 15:14:40 -0400

    Updated books.js

--- app/public/js/books.js
+++ app/public/js/books.js
@@ -24,9 +24,16 @@ const SomeApp = {
                 console.error(err);
             })
         },
+        postBook(evt) {
+            if (this.selectedBook === null) {
+                this.postNewBook(evt);
+            } else {
+                this.postEditBook(evt);
+            }
+        },
         postNewBook(evt) {
             //this.bookForm.bookId = this.selectedBook.id;
-            console.log("Posting:", this.bookForm);
+            console.log("Creating!", this.bookForm);
             //alert("Posting!");
 
             fetch('api/book/create.php',{
@@ -44,9 +51,56 @@ const SomeApp = {
 
                //reset the form
                this.bookForm = {};
+               this.resetBookForm();
            }); 
+        },
+        postEditBook(evt) {
+            this.bookForm.bookId = this.selectedBook.id;
+            this.offerForm.id = this.selecteBook.id;
+
+            console.log("Updating!", this.bookForm);
+
+            fetch('api/book/update.php', {
+                method:'POST',
+                body: JSON.stringify(this.bookForm),
+                headers: {
+                    "Content-Type": "application/json; charset=utf-8"
+                }
+            })
+            .then(response => response.json() )
+            .then( json => {
+                console.log("Returned from post:", json);
+                //TODO: test a result was returned!
+                this.book = json;
+
+                this.resetBookForm();
+            });
+        },
+        postDeleteBook(b) {
+            if (!confirm("Are you sure you want to delete the book from "+b.bk.title+"?")) {
+                return;
+            }
+
+            fetch('api/offer/delete.php', {
+                method: 'POST',
+                body: JSON.stringify(b),
+                headers: {
+                    "Content-Type": "application/json; charset=utf-8"
+                }
+            })
+            .then(response => response.json() )
+            .then( json => {
+                console.log("Returned from post:", json);
+                //TODO: test a result was returned!
+                this.book = json;
+
+                this.resetBookForm();
+            });
+        },
+        selectBook(b) {
+            this.selectedBook = null;
+            this.bookForm = {};
         }
-    },
     created() {
         this.fetchBooksData();
     }
